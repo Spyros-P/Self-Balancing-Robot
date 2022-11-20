4b5113cc4c9d226c0e8f6da3f815ddeff0c6bb64
Spyros-P <56654089+Spyros-P@users.noreply.github.com>
2022-11-20 02:40:53 +0200

    Update robotaki.ino

--- self-balancing-robot/robotaki/robotaki.ino
+++ self-balancing-robot/robotaki/robotaki.ino
@@ -2,103 +2,57 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00010";
+const byte address[6] = "Ntu@X";
 
 #include <MPU6500_WE.h>
 #include <Wire.h>
 #define MPU6500_ADDR 0x68
 
-#include <Servo.h>
-Servo myservo;  // create servo object to control a servo
+//#include <Servo.h>
+//Servo myservo;  // create servo object to control a servo
 
-//#define PRINT 0
 #define RAD_TO_DEG 57.295779513082320876798154814105
 
 MPU6500_WE myMPU6500 = MPU6500_WE(MPU6500_ADDR);
 
-//int servo_remember = 90;
-
-double setpoint = 335;
-double error = 0, prev_error = 0;
-bool button_status = false;
-bool power_off = true;
+double error = 0;
 int output = 0;
-double theta = 0;
 double theta_deg = 0;
 
 double sum = 0;
 
-double K[] = {0,0};
+struct Operating_Data {
+  double Kp = 14;
+  double Kd = 3;
+  double Ki = 8;
+  double setpoint = 2.2;
+  double sum_limit = 50;
+  double exp_dec_sum = 0.97;
+  double exp_inc_sum = 1.01;
+} Default_Data, Received_Data, *get_Data;
+
 
 // the setup routine runs once when you press reset:
 void setup() {
   Serial.begin(115200);
   //Serial.println("STARTING !!!");
-  radio.begin();
-  radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
-  radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
-  radio.setDataRate(RF24_250KBPS);
-  radio.startListening();              //This sets the module as receiver
+  radio_setup();
 
-  myservo.attach(9);
-  myservo.write(90);
-  pinMode(10, OUTPUT);
-  digitalWrite(10,LOW);
+  //myservo.attach(9);
+  //myservo.write(90);
   
-#ifdef PRINT
-  //Serial.begin(115200);
-#endif
-  Wire.begin();
-
-#ifdef PRINT
-  if(!myMPU6500.init()){
-    //Serial.println("MPU6500 does not respond");
-  }
-  else{
-    //Serial.println("MPU6500 is connected");
-  }
-  //Serial.println("Position you MPU6500 flat and don't move it - calibrating...");
-#endif
-
-  for (int i=0; i<5 ;i++) {
-    delay(200);
-    digitalWrite(10,LOW);
-    delay(200);
-    digitalWrite(10,HIGH);
-  }
-  delay(200);
-  digitalWrite(10,HIGH);
-  
-  myMPU6500.autoOffsets();
-
-#ifdef PRINT
-  //Serial.println("Done!");
-#endif
-
-  //myMPU6500.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
-  //myMPU6500.setGyrOffsets(45.0, 145.0, -105.0);
-  myMPU6500.enableGyrDLPF();
-  //myMPU6500.disableGyrDLPF(MPU6500_BW_WO_DLPF_8800); // bandwdith without DLPF
-
-  myMPU6500.setGyrDLPF(MPU6500_DLPF_6);
-  myMPU6500.setSampleRateDivider(0);
-  myMPU6500.setGyrRange(MPU6500_GYRO_RANGE_250);
-  myMPU6500.setAccRange(MPU6500_ACC_RANGE_2G);
-  myMPU6500.enableAccDLPF(true);
-  myMPU6500.setAccDLPF(MPU6500_DLPF_6);
+  MPU_setup();
   
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
-  pinMode(6, INPUT);
   
 
   digitalWrite(2, LOW);
   digitalWrite(3, LOW);
   digitalWrite(4, LOW);
   digitalWrite(5, LOW);
-  digitalWrite(10, HIGH);
 }
 
 // the loop routine runs over and over again forever:
@@ -134,87 +88,84 @@ void loop() {
 
   
   if (radio.available()) {
-    radio.read(&K, sizeof(K));
-    //Serial.print("Kp: ");
-    //Serial.print(K[0]);
-    //Serial.print(", Kd: ");
-    //Serial.println(K[1]);
+    radio.read(&Received_Data, sizeof(Received_Data));
+    get_Data = &Received_Data;
   }
-  //else {
-    //Serial.println("Could not connect!");
-  //}
-  
-  //Serial.println("Attempting to read...");
+  else
+    get_Data = &Default_Data;
+
   xyzFloat gValue = myMPU6500.getGValues();
-  //Serial.println("Read succesfully!");
+  xyzFloat gyr = myMPU6500.getGyrValues();
+
+             
+  if(gValue.z != 0){
+    //gValue.z = 2 - gValue.z;
+    theta_deg = atan2(gValue.x, gValue.z)*RAD_TO_DEG;
+  }
+
+  error = theta_deg - get_Data->setpoint;
+  
+  //Serial.print("MIN:-90, MAX:90, angle:");
+  //Serial.println(theta_deg);
+
+  if (sum*theta_deg<0)
+    sum = error + get_Data->exp_dec_sum*sum;
+  else
+    sum = error + get_Data->exp_inc_sum*sum;
+  
+  sum = min(max(-get_Data->sum_limit,sum),get_Data->sum_limit);
+
+  output = get_Data->Kp*error - get_Data->Kd*gyr.y + get_Data->Ki*sum;
+
+  drive_motors(output);
+}
 
 
-  //Serial.println("Acceleration in g (x,y,z):");
-  //Serial.print(gValue.x);
-  //Serial.print("   ");
-  //Serial.print(gValue.y);
-  //Serial.print("   ");
-  //Serial.println(gValue.z);
-    
-    
-    if(gValue.z != 0){
-      gValue.z = 2 - gValue.z;
-      theta = atan(gValue.x/gValue.z);
-      theta_deg = 0.4*theta_deg + 0.6*atan(gValue.x/gValue.z)*RAD_TO_DEG;
-      //theta = sin(atan(gValue.x/gValue.z));
-    }  
-    
-    
-#ifdef PRINT
-    //Serial.print("angle: ");
-    //Serial.println(theta);
-#endif
 
-    prev_error = error;
-    error = theta_deg;
-    Serial.print("MIN:-90, MAX:90, angle:");
-    Serial.println(theta_deg);
+inline void radio_setup() {
+  radio.begin();
+  radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
+  radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.setDataRate(RF24_250KBPS);
+  radio.startListening();              //This sets the module as receiver
+}
 
-    sum = 0.9*error + 0.3*sum;
+inline void MPU_setup() {
+  Wire.begin();
 
-    sum = min(max(-40,sum),40);
+  myMPU6500.setAccOffsets(-14240.0, 18220.0, -17280.0, 15590.0, -20930.0, 12080.0);
+  myMPU6500.setGyrOffsets(45.0, 145.0, -105.0);
+  myMPU6500.enableGyrDLPF();
+  //myMPU6500.disableGyrDLPF(MPU6500_BW_WO_DLPF_8800); // bandwdith without DLPF
 
-    xyzFloat gyr = myMPU6500.getGyrValues();
+  myMPU6500.setGyrDLPF(MPU6500_DLPF_4);
+  myMPU6500.setSampleRateDivider(0);
+  myMPU6500.setGyrRange(MPU6500_GYRO_RANGE_250);
+  myMPU6500.setAccRange(MPU6500_ACC_RANGE_2G);
+  myMPU6500.enableAccDLPF(true);
+  myMPU6500.setAccDLPF(MPU6500_DLPF_4);
 
-    output = K[0]*error + K[1]*gyr.y;//(error-prev_error);
-    //Serial.print("MIN:-250, MAX:250, GYR_y:");
-    //Serial.println(gyr.y);
+  //myMPU6500.autoOffsets();
+}
 
-#ifdef PRINT
-    //Serial.print("output: ");
-    //Serial.println(output);
-#endif
+inline void drive_motors(int output) {
+  output *= -1;
+  if (output >= 0) {
+    digitalWrite(2, LOW);
+    digitalWrite(4, LOW);
 
-    //output *= 4 /(abs(output)/10+1)+1;
-    //output *= 4/(abs(output)/10+1)+1;
+    output = min(output,255);
 
+    analogWrite(3,output);
+    analogWrite(5,output*0.95);
+  }
+  else {
+    digitalWrite(2, HIGH);
+    digitalWrite(4, HIGH);
 
-    //Serial.println(output);
+    output = min(-output,255);
 
-
-    if (output >= 0) {
-      digitalWrite(2, LOW);
-      digitalWrite(4, LOW);
-
-      output = min(output,255);
-
-      analogWrite(3,output*0.95);
-      analogWrite(5,output);
-    }
-    else {
-      digitalWrite(2, HIGH);
-      digitalWrite(4, HIGH);
-
-      output = min(-output,255);
-
-      analogWrite(3,255-output*0.95);
-      analogWrite(5,255-output);
-    }
-    //delay(40);
-    
+    analogWrite(3,255-output);
+    analogWrite(5,255-output*0.95);
+  }
 }
