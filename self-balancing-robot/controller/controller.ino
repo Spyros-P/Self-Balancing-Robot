25b505341d0369bb6d4e58f8d1d0e4fc1687aaf7
Spyros-P <56654089+Spyros-P@users.noreply.github.com>
2022-11-25 12:58:06 +0200

    Update controller.ino

--- self-balancing-robot/controller/controller.ino
+++ self-balancing-robot/controller/controller.ino
@@ -1,26 +1,35 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 RF24 radio(7, 8); // CE, CSN         
 const byte address[6] = "Ntu@X";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 #define Kp_MAX 50.0
 #define Kd_MAX 10.0
 #define Ki_MAX 15.0
 
+unsigned long prev_time = 0;
+
 struct Operating_Data {
-  double Kp = 14;
+  double Kp = 7;
   double Kd = 3;
-  double Ki = 8;
-  double setpoint = 2.5;
-  double sum_limit = 120;
-  double sum_weight = 0.6;
+  double Ki = 3;
+  double setpoint = 1.65;
+  double sum_limit = 80;
+  double sum_weight = 0.5;
   double exp_dec_sum = 0.97;
   double exp_inc_sum = 1.03;
 } Transmitting_Data;
 
 void setup() {
-  Serial.begin(115200);
+	lcd.begin();
+	lcd.backlight();
+  //Serial.begin(115200);
   radio.begin();                  //Starting the Wireless communication
   radio.openWritingPipe(address); //Setting the address where we will send the data
   radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
@@ -31,15 +40,33 @@ void setup() {
 
 void loop() {
 
-  Transmitting_Data.Kp = analogRead(0)*Kp_MAX/1023;
-  Transmitting_Data.Kd = analogRead(1)*Kd_MAX/1023;
-  Transmitting_Data.Ki = analogRead(2)*Ki_MAX/1023;
+  Transmitting_Data.Kp = (1023-analogRead(0))*Kp_MAX/1023;
+  Transmitting_Data.Kd = (1023-analogRead(1))*Kd_MAX/1023;
+  Transmitting_Data.Ki = (1023-analogRead(2))*Ki_MAX/1023;
 
   radio.write(&Transmitting_Data, sizeof(Transmitting_Data));
 
   //delay(1);
 
+  if (millis() - prev_time > 150 ) {
+    lcd.setCursor(0,0);
+    lcd.print("Kp:");
+    if(Transmitting_Data.Kp<10) lcd.print(" ");
+    lcd.print(Transmitting_Data.Kp);
+    lcd.setCursor(9,0);
+    lcd.print("Kd:");
+    lcd.print(Transmitting_Data.Kd);
+    lcd.setCursor(0,1);
+    lcd.print("Ki:");
+    if(Transmitting_Data.Ki<10) lcd.print(" ");
+    lcd.print(Transmitting_Data.Ki);
+    lcd.setCursor(9,1);
+    lcd.print("st:");
+    lcd.print(Transmitting_Data.setpoint);
+   prev_time = millis();
+  }
 
+/*
   Serial.print("Kp:");
   Serial.print(Transmitting_Data.Kp);
   Serial.print(", Kd:");
@@ -48,5 +75,5 @@ void loop() {
   Serial.print(Transmitting_Data.Ki);
   Serial.print(", setpoint:");
   Serial.println(Transmitting_Data.setpoint);
-
+*/
 }
